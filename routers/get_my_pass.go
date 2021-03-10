6284b126586628d179098ea5d614cd6c2015d264
blotin1993 <blottagonzalo@gmail.com>
2021-03-10 14:01:18 -0300

    added mail validation

--- routers/get_my_pass.go
+++ routers/get_my_pass.go
@@ -12,20 +12,24 @@ import (
 func GetMyPassword(w http.ResponseWriter, r *http.Request) {
 	email := r.URL.Query().Get("email")
 	if len(email) < 1 {
-		http.Error(w, "debe enviar el parametro email", http.StatusBadRequest)
+		http.Error(w, "must complete email form", http.StatusBadRequest)
 		return
 	}
-	user, _, _ := db.UserAlreadyExist(email)
+	user, mailExist, _ := db.UserAlreadyExist(email)
+	if !mailExist {
+		http.Error(w, "Wrong mail.", 400)
+		return
+	}
 	decryptedPassword, err := db.DecryptPassword(user.Password)
 	if err != nil {
-		http.Error(w, "Ocurrio un error al desencriptar la contrasena"+err.Error(), 400)
+		http.Error(w, "Internal error"+err.Error(), 400)
 		return
 	}
 	msg := gomail.NewMessage()
 	msg.SetHeader("From", "vlotingaming@gmail.com")
 	msg.SetHeader("To", email)
 	msg.SetHeader("Subject", "Get your password.")
-	msg.SetBody("text/html", "Hola <b><i>Lucas</i></b>/n"+
+	msg.SetBody("text/html", "Hola <b><i>"+user.Name+"</i></b>!\n"+
 		"Recibimos tu pedido de recuperacion de password! \n Your Password: "+decryptedPassword+
 		" \n\n Esperamos que sea de tu ayuda, estamos a tu disposicion! \n\n Feedback-Api Admin\n <i>feedbackapiadm@gmail.com</i>\n "+
 		time.Now().Format("2006.01.02 15:04:05"))
@@ -33,7 +37,7 @@ func GetMyPassword(w http.ResponseWriter, r *http.Request) {
 	//Send the email to user
 	d := gomail.NewPlainDialer("smtp.gmail.com", 587, "vlotingaming@gmail.com", "V2H!x%CaxCeM")
 	if err := d.DialAndSend(msg); err != nil {
-		http.Error(w, "Ocurrio un error al enviar el mail"+err.Error(), 400)
+		http.Error(w, "An error has ocurred sending the email"+err.Error(), 400)
 		return
 	}
 	w.WriteHeader(http.StatusCreated)

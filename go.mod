5639614169bfa881e44f709db462b78ffe2c4a07
blotin1993 <blottagonzalo@gmail.com>
2021-03-09 22:19:58 -0300

    corrected some typos

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/fatih/structs v1.1.0 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.3.0
 	github.com/rs/cors v1.7.0
--- routers/feedback_try.go
+++ routers/feedback_try.go
@@ -19,99 +19,21 @@ func FeedbackTry(w http.ResponseWriter, r *http.Request) {
 	var fb models.Feedback
 
 	err := json.NewDecoder(r.Body).Decode(&fb)
-	//
+
+	//-------Feedback validation------
+	// n := structs.Map(fb.PerformanceArea)
+	// fmt.Println(n)
+	// httpServer := fb.Field("Server").Fields()
+
+	// for x, v := range n {
+	// 	fmt.Println(x, v)
+	// }
+
+	//-----------------------------------
+
 	fb.IssuerID = IDUser
 	fb.ReceiverID = rID
 	fb.Date = time.Now()
-
-	//-------Feedback validation------<<<<<<<<<<<<<<<<<<<<<<<<<<<<< TO IMPROVE
-	if len(fb.TeamArea.Message) == 0 {
-		http.Error(w, "the message must have at least one character", 400)
-		return
-	}
-	if len(fb.TeamArea.Message) >= 500 {
-		http.Error(w, "must have a maximum of 500 characters", 400)
-		return
-	}
-
-	if len(fb.TeamArea.TeamPlayer) == 0 {
-		http.Error(w, "empty field", 400)
-		return
-	}
-
-	if len(fb.TeamArea.Commited) == 0 {
-		http.Error(w, "empty field", 400)
-		return
-	}
-
-	if len(fb.TeamArea.Communication) == 0 {
-		http.Error(w, "empty field", 400)
-		return
-	}
-
-	//validation TechArea
-	if len(fb.TechArea.Message) == 0 {
-		http.Error(w, "the message must have at least one character", 400)
-		return
-	}
-
-	if len(fb.TechArea.Message) >= 500 {
-		http.Error(w, "must have a maximum of 500 characters", 400)
-		return
-	}
-
-	if len(fb.TechArea.BestPractices) == 0 {
-		http.Error(w, "empty field", 400)
-		return
-	}
-
-	if len(fb.TechArea.TechKnowledge) == 0 {
-		http.Error(w, "empty field", 400)
-		return
-	}
-
-	if len(fb.TechArea.CodingStyle) == 0 {
-		http.Error(w, "empty field", 400)
-		return
-	}
-
-	//validation PerformanceArea
-	if len(fb.PerformanceArea.Message) == 0 {
-		http.Error(w, "the message must have at least one character", 400)
-		return
-	}
-
-	if len(fb.PerformanceArea.Message) >= 500 {
-		http.Error(w, "must have a maximum of 500 characters", 400)
-		return
-	}
-
-	if len(fb.PerformanceArea.Message) == 0 {
-		http.Error(w, "empty field", 400)
-		return
-	}
-
-	if len(fb.PerformanceArea.Message) == 0 {
-		http.Error(w, "empty field", 400)
-		return
-	}
-
-	if len(fb.PerformanceArea.Message) == 0 {
-		http.Error(w, "empty field", 400)
-		return
-	}
-
-	//Validation Message
-	if len(fb.Message) == 0 {
-		http.Error(w, "the message must have at least one character", 400)
-		return
-	}
-
-	if len(fb.Message) >= 1500 {
-		http.Error(w, "must have a maximum of 1500 characters", 400)
-		return
-	}
-	//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 	_, status, err := db.AddFeedback(fb)
 

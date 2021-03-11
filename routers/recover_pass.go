c3cb13b9c0974a3b7b2f2ed9ef49663ce0b6f242
blotin1993 <blottagonzalo@gmail.com>
2021-03-10 22:02:06 -0300

    added some params to sendemail

--- routers/recover_pass.go
+++ routers/recover_pass.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/blotin1993/feedback-api/db"
 	services "github.com/blotin1993/feedback-api/services/email"
-	"gopkg.in/gomail.v2"
 )
 
 //GetMyPassword - receive the user data from DataBase and send an Email with his current password
@@ -26,15 +25,13 @@ func GetMyPassword(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Internal error"+err.Error(), 400)
 		return
 	}
-	//create msg
-	msg := gomail.NewMessage()
-	//Body set
-	msg.SetBody("text/html", "Hola <b><i>"+user.Name+"</i></b>!\n"+
-		"Recibimos tu pedido de recuperacion de password! \n Your Password: "+decryptedPassword+
-		" \n\n Esperamos que sea de tu ayuda, estamos a tu disposicion! \n\n Feedback-Api Admin\n <i>feedbackapiadm@gmail.com</i>\n "+
-		time.Now().Format("2006.01.02 15:04:05"))
+	bodyString := "Hola <b><i>" + user.Name + "</i></b>!\n" +
+		"Recibimos tu pedido de recuperacion de password! \n Your Password: " + decryptedPassword +
+		" \n\n Esperamos que sea de tu ayuda, estamos a tu disposicion! \n\n Feedback-Api Admin\n <i>feedbackapiadm@gmail.com</i>\n " +
+		time.Now().Format("2006.01.02 15:04:05")
+
 	//Email send function
-	if !services.SendEmail(msg, email, "Get your password.") {
+	if !services.SendEmail(email, "Get your password.", bodyString) {
 		http.Error(w, "An error has ocurred sending the email"+err.Error(), 400)
 		return
 	}

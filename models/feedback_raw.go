c1e229e49fd2d4ef4a45fe775e0b2083dfc696a5
blotin1993 <blottagonzalo@gmail.com>
2021-03-10 23:31:09 -0300

    optimized models and db storage

--- models/feedback_raw.go
+++ models/feedback_raw.go
@@ -2,22 +2,8 @@ package models
 
 //FeedbackRaw structure
 type FeedbackRaw struct {
-	TechArea struct {
-		Message       string `bson:"tamessage,omitempty" json:"tamessage"`
-		TechKnowledge string `bson:"techKnowledge,omitempty" json:"techknowledge"`
-		BestPractices string `bson:"bestPractices,omitempty" json:"bestpractices"`
-		CodingStyle   string `bson:"codingStyle,omitempty" json:"codingstyle"`
-	}
-	TeamArea struct {
-		Message       string `bson:"temessage,omitempty" json:"temessage"`
-		TeamPlayer    string `bson:"teamPlayer,omitempty" json:"teamplayer"`
-		Commited      string `bson:"commited,omitempty" json:"commited"`
-		Communication string `bson:"communication,omitempty" json:"communication"`
-	}
-	PerformanceArea struct {
-		Message        string `bson:"pamessage,omitempty" json:"pamessage"`
-		WorkQuality    string `bson:"workQuality,omitempty" json:"workquality"`
-		ClientOriented string `bson:"clientOriented,omitempty" json:"clientoriented"`
-	}
-	Message string `bson:"message,omitempty" json:"message"`
+	TechArea        TechArea        `bson:"techarea,omitempty,inline" json:"techarea,omitempty"`
+	TeamArea        TeamArea        `bson:"teamarea,omitempty,inline" json:"teamarea,omitempty"`
+	PerformanceArea PerformanceArea `bson:"performancearea,omitempty,inline" json:"performancearea,omitempty" `
+	Message         string          `bson:"message,omitempty" json:"message"`
 }

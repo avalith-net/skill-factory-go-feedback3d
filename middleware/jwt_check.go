7f521b462e40591c6514f1c070420b1433b6024b
blotin1993 <blottagonzalo@gmail.com>
2021-03-12 00:56:10 -0300

    corrected some error handling funcs

--- middleware/jwt_check.go
+++ middleware/jwt_check.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"net/http"
+
 	"github.com/blotin1993/feedback-api/routers"
 	"github.com/gin-gonic/gin"
 )
@@ -10,8 +12,7 @@ func ValidateJWT() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		_, _, _, err := routers.TokenProcess(c.GetHeader("Authorization"))
 		if err != nil {
-			// http.Error(w, "Token error."+err.Error(), http.StatusBadRequest)
-			c.AbortWithStatusJSON(500, gin.H{"message": "Token error."})
+			c.String(http.StatusInternalServerError, "Token error.")
 			return
 		}
 		c.Next()

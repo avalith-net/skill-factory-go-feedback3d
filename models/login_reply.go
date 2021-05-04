16e645c2d933667a2234f47feb41bf5ee2acca9e
Alejandro <alesantin012@hotmail.com>
2021-05-04 14:58:34 -0300

    get user info when login

--- models/login_reply.go
+++ models/login_reply.go
@@ -2,5 +2,6 @@ package models
 
 //LoginReply model used to answer login attempt.
 type LoginReply struct {
-	Token string `json:"token,omitempty"`
+	Token      string `json:"token,omitempty"`
+	UserLogged User   `json:"user_logged,omitempty"`
 }
